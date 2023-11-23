c474f74130a24a660c10e222ccb6d20de2a996a5
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-23 17:22:30 -0300

    Creacion sitio web

--- sitio_web/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.shortcuts import render
-
-def home(request):
-
-    return render(request, 'home.html') 
--- sitio_web/views.py
+++ sitio_web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.db import models
+from django.shortcuts import render
+
+def home(request):
+
+    return render(request, 'sitio_web/home.html') 
