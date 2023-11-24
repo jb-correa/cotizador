974400355becd209fe3577f310122665a8d9a971
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-24 10:54:00 -0300

    Creacion del modelo producto

--- sitio_web/admin.py
+++ sitio_web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Producto
 
-# Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "nombre", "precios", "precio_promedio")
+    
+
+admin.site.register(Producto, ProductoAdmin)
---
+++ sitio_web/models.py
@@ -0,0 +1,29 @@
+import json
+from django.db import models
+from django.forms import ModelForm
+
+class Producto(models.Model):
+    nombre=models.CharField(max_length=50)
+    precio_promedio=models.FloatField(null=True)
+    created=models.DateField(auto_now_add=True)  
+    precios = models.FloatField(max_length=200, null=True)
+
+    def set_precios(self, x):
+        self.precios = json.dumps(x)
+
+    def set_precios(self):
+        return json.loads(self.precios)
+    class Meta:
+        db_table='productos'
+        ordering=['created']
+        verbose_name='producto'
+        verbose_name_plural='productos'
+    def __str__(self):
+        return f'Busqueda del producto {self.nombre}, de la fecha {self.created}'
+    
+
+class productoForm(ModelForm):
+    class Meta:
+        model=Producto
+        fields=['nombre',]
+    
