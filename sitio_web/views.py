35db1f08d6b48013d8c7d9270c2580b089553578
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-11-27 18:02:54 -0300

    Trabajo en sitio_web.views

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import productoForm, Producto
 
 def home(request):
-
+    form=productoForm()
+    if request.method == 'POST':
+        form=productoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect("Home")
+        
     return render(request, 'sitio_web/home.html') 
