66f8314b8ad234b515883d6976afeaeb7fa957f3
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-04 12:03:42 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -1,19 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-from xlutils.copy import copy
-import re
-import xlwt
-import time
-import xlrd
-from webdriver_manager.chrome import ChromeDriverManager
-import unittest
-from selenium.webdriver.support.wait import WebDriverWait
-import tracemalloc
+from selenium.webdriver.common.by import By
 from .models import Producto
 
-
-
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
