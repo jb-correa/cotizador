dc92239b0b6f5c7fcd530a5b3edf3de8b1c83131
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-06 13:24:41 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -7,5 +7,8 @@ driver.get("http://www.mercadolibre.com.ar")
 input=driver.find_element(By.ID,"cb1-edit")
 input.click()
 input.send_keys("telefono samsung")
+button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
+
+button.click()
 driver.implicitly_wait(5)
 driver.close()
