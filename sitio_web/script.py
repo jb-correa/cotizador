3008a4853ce29841c811ea6924f5ddce104fd472
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-07 11:28:24 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -5,6 +6,7 @@ import re
 
 driver = webdriver.Chrome()
 driver.get("http://www.mercadolibre.com.ar")
+driver.maximize_window()
 input=driver.find_element(By.ID,"cb1-edit")
 input.click()
 input.send_keys("telefono samsung")
@@ -13,11 +15,11 @@ button.click()
 result=driver.find_element(By.XPATH,"//h2[contains(text(),'Samsung Galaxy A33 5g 128 Gb Awesome Black 6 Gb Ra')]")
 result.click()
 driver.switch_to.new_window('tab')
+
 html=driver.page_source
-numero=re.search(r'\d', html)
-print(numero)
+print(len(html))
+
 
 #Encontrar la forma de ubicar primeros cinco elementos
 
-driver.implicitly_wait(5)
 driver.close()
