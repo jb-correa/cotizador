b2fe6d670f06011908b81d4b3c18bb770bb80d1d
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-07 11:11:27 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -9,6 +9,11 @@ input.click()
 input.send_keys("telefono samsung")
 button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
 button.click()
+result=driver.find_element(By.XPATH,"//h2[contains(text(),'Samsung Galaxy A33 5g 128 Gb Awesome Black 6 Gb Ra')]")
+result.click()
+driver.switch_to.new_window('tab')
+html=driver.page_source
+print(html)
 
 #Encontrar la forma de ubicar primeros cinco elementos
 
