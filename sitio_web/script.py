01876c525fe06a7083072d5d0bf1b6bf3a1da6fb
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-14 11:02:00 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -35,11 +35,10 @@ print(precios)
 #Solucionar esto antes de encontrar promedio y elnazar con views
 maximo=max(precios)
 print(maximo)
-tope_minimo=maximo*0.3
+tope_minimo=maximo*0.2
 print(tope_minimo)
 for precio in precios:
     if(precio < tope_minimo):
-        print(precio)
         precios.remove(precio)
 
 print(precios)
