1db808301bb8a94b5f4e8a7d7b155c8ab5815e88
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-14 11:07:24 -0300

    Trabajo en script

--- sitio_web/script.py
+++ sitio_web/script.py
@@ -5,42 +5,45 @@ from selenium.webdriver.common.by import By
 import re
 import time
 
-def busqueda():
-    pass
+def busqueda(producto):
+    driver = webdriver.Chrome()
+    driver.get("http://www.mercadolibre.com.ar")
+    driver.maximize_window()
+    input=driver.find_element(By.ID,"cb1-edit")
+    input.click()
+    input.clear()
 
-driver = webdriver.Chrome()
-driver.get("http://www.mercadolibre.com.ar")
-driver.maximize_window()
-input=driver.find_element(By.ID,"cb1-edit")
-input.click()
-input.clear()
+    #Aqui hay que insertar el nombre del objeto ingresado en el sitio
+    input.send_keys(producto.nombre_producto)
+    button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
+    button.click()
 
-#Aqui hay que insertar el nombre del objeto ingresado en el sitio
-input.send_keys("telefono samsung")
-button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
-button.click()
+    precios=[]
+    numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
 
-precios=[]
-numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
+    #Quita el punto del precio y convierte en int
+    for numero in numeros:
+        numero=numero.text.replace(".", "")
+        numero=int(numero)
+        precios.append(numero)
 
-#Quita el punto del precio y convierte en int
-for numero in numeros:
-    numero=numero.text.replace(".", "")
-    numero=int(numero)
-    precios.append(numero)
+    print(precios)
 
-print(precios)
+    #Quita precios falopa sacando menores al 30% del maximo
+    #Solucionar esto antes de encontrar promedio y elnazar con views
+    maximo=max(precios)
+    producto.precio_maximo=maximo
+    print(maximo)
+    minimo=min(precios)
+    producto.precio_minimo=minimo  
+    tope_minimo=maximo*0.2
+    print(tope_minimo)
+    for precio in precios:
+        if(precio < tope_minimo):
+            precios.remove(precio)
 
-#Quita precios falopa sacando menores al 30% del maximo
-#Solucionar esto antes de encontrar promedio y elnazar con views
-maximo=max(precios)
-print(maximo)
-tope_minimo=maximo*0.2
-print(tope_minimo)
-for precio in precios:
-    if(precio < tope_minimo):
-        precios.remove(precio)
+    print(precios)
 
-print(precios)
+    driver.close()
 
-driver.close()
+    return producto
