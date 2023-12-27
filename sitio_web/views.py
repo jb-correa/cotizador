4d9080051f0c8fbaa8a7c4ffdf8661bcdfb88927
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-12-27 13:58:05 -0300

    Trabajo en views.py

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -5,7 +5,7 @@ from .models import productoForm, Producto
 from .script import busqueda
 from django.http import JsonResponse
 import httpx
-import script
+from selenium import webdriver
 
 def home(request):
     form=productoForm()
@@ -22,12 +22,55 @@ def home(request):
     return render(request, 'sitio_web/home.html', {"form": form}) 
 
 
-async def busqueda(request):
+async def busqueda(request, producto):
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(script.busqueda)
-        data = response.json()
-    return JsonResponse(data)
+        driver = webdriver.Chrome()
+        driver.get("http://www.mercadolibre.com.ar")
+        driver.maximize_window()
+        input=driver.find_element(By.ID,"cb1-edit")
+        input.click()
+        input.clear()
+
+        #Aqui hay que insertar el nombre del objeto ingresado en el sitio
+        input.send_keys(producto.nombre_producto)
+        button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
+        button.click()
+
+        precios=[]
+        numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
+
+        #Quita el punto del precio y convierte en int
+        for numero in numeros:
+            numero=numero.text.replace(".", "")
+            numero=int(numero)
+            precios.append(numero)
+
+        print(precios)
+
+        #Quita precios falopa sacando menores al 30% del maximo
+        #Solucionar esto antes de encontrar promedio y elnazar con views
+        maximo=max(precios)
+        producto.precio_maximo=maximo
+        print(maximo)
+        minimo=min(precios)
+        producto.precio_minimo=minimo  
+        tope_minimo=maximo*0.2
+        print(tope_minimo)
+        for precio in precios:
+            if(precio < tope_minimo):
+                precios.remove(precio)
+
+        print(precios)
+        producto.precio_promedio=sum(precios)/len(precios)
+        producto.precios(precios)
+    
+
+        driver.close()
+
+        return producto
+
+   
     
 
     
