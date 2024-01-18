b3a5bee9ea968dc6f27bb09e2815087d7ce6c505
Juan Bautista Correa <jbautistacorrea@gmail.com>
2024-01-18 14:12:37 -0300

    Cambios en estructura

--- sitio_web/urls.py
+++ sitio_web/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('',views.home, name="Home"),
     path('resultado', views.resultado, name="Resultado"),
-    path('loading', views.loading, name="Loading")
 ]
