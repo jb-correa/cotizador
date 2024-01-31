d61deab32f03c29227a447c6985db6d4ce42348c
Juan Bautista Correa <jbautistacorrea@gmail.com>
2024-01-31 14:05:28 -0300

    Cambios en views.py

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -27,45 +27,43 @@ async def busqueda(request):
 
     productos=Producto.objects.all()
     producto=productos[len(productos)-1]
-    async with httpx.AsyncClient():
-        driver = webdriver.Chrome()
-        driver.get("http://www.mercadolibre.com.ar")
-        driver.maximize_window()
-        input=driver.find_element(By.ID,"cb1-edit")
-        input.click()
-        input.clear()
+    driver = webdriver.Chrome()
+    driver.get("http://www.mercadolibre.com.ar")
+    driver.maximize_window()
+    input=driver.find_element(By.ID,"cb1-edit")
+    input.click()
+    input.clear()
+       
         
-        
-        input.send_keys('telefono samsung')
-        button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
-        button.click()
+    input.send_keys('telefono samsung')
+    button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
+    button.click()
 
-        precios=[]
-        numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
+    precios=[]
+    numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
 
-        #Metodo que quita el punto del precio y convierte en int
-        for numero in numeros:
-            numero=numero.text.replace(".", "")
-            numero=int(numero)
-            precios.append(numero)
+    #Metodo que quita el punto del precio y convierte en int
+    for numero in numeros:
+        numero=numero.text.replace(".", "")
+        numero=int(numero)
+        precios.append(numero)
 
-        #Quita precios falopa sacando menores al 30% del maximo
-        #No estaría funcionando
-        maximo=max(precios)
-        producto.precio_maximo=maximo
-        minimo=min(precios)
-        producto.precio_minimo=minimo  
-        tope_minimo=maximo*0.2
-        for precio in precios:
-            if(precio < tope_minimo):
-                precios.remove(precio)
+    #Quita precios falopa sacando menores al 30% del maximo
+    #No estaría funcionando
+    maximo=max(precios)
+    producto.precio_maximo=maximo
+    minimo=min(precios)
+    producto.precio_minimo=minimo  
+    tope_minimo=maximo*0.2
+    for precio in precios:
+        if(precio < tope_minimo):
+            precios.remove(precio)
 
-        producto.precio_promedio=sum(precios)/len(precios)
-        producto.precios(precios)
+    producto.precio_promedio=sum(precios)/len(precios)
+    producto.precios(precios)
     
-        producto.save()
-
-        driver.close()
+    producto.save()
+    driver.close()
 
 
 def resultado(request):
