7293e864edc9ae8f524c2577ff5ba90901747d27
Juan Bautista Correa <jbautistacorrea@gmail.com>
2024-02-07 15:03:09 -0300

    Busqueda el error en views.py

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -10,12 +10,12 @@ from selenium import webdriver
 #Landing page
 def home(request):
     form=productoForm()
+    producto=Producto()
     if request.method == 'POST':
         form=productoForm(request.POST)
         if form.is_valid():
-            producto=Producto()
-            producto=form.save()
             
+            producto=form.save()
             
             return ("Resultado",{"producto": producto})
         
