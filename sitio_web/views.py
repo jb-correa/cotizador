c3dfc9ac9fec80fe1be9745d6d56dba9ca188673
Juan Bautista Correa <jbautistacorrea@gmail.com>
2024-02-29 17:40:13 -0300

    Cambios en views.py

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -23,41 +23,41 @@ def home(request):
 
 
 #Método asincrónico para busqueda de precios
-#def busqueda(request):
+def busqueda(request):
 
-#    productos=Producto.objects.all()
-#    producto=productos[len(productos)-1]
-#    driver = webdriver.Chrome()
-#    driver.get("http://www.mercadolibre.com.ar")
-#    driver.maximize_window()
-#    input=driver.find_element(By.ID,"cb1-edit")
-#    input.click()
-#    input.clear()
+    productos=Producto.objects.all()
+    producto=productos[len(productos)-1]
+    driver = webdriver.Chrome()
+    driver.get("http://www.mercadolibre.com.ar")
+    driver.maximize_window()
+    input=driver.find_element(By.ID,"cb1-edit")
+    input.click()
+    input.clear()
        
         
-#    input.send_keys('telefono samsung')
-#    button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
-#    button.click()
+    input.send_keys('telefono samsung')
+    button=driver.find_element(By.XPATH,"//div[@aria-label='Buscar']")
+    button.click()
 
-#    precios=[]
-#    numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
+    precios=[]
+    numeros=driver.find_elements(By.CLASS_NAME, "andes-money-amount__fraction")
 
-    #Metodo que quita el punto del precio y convierte en int
-#    for numero in numeros:
-#        numero=numero.text.replace(".", "")
-#        numero=int(numero)
-#        precios.append(numero)
+   #Metodo que quita el punto del precio y convierte en int
+    for numero in numeros:
+        numero=numero.text.replace(".", "")
+        numero=int(numero)
+        precios.append(numero)
 
     #Quita precios falopa sacando menores al 30% del maximo
     #No estaría funcionando
-#    maximo=max(precios)
-#    producto.precio_maximo=maximo
-#    minimo=min(precios)
-#    producto.precio_minimo=minimo  
-#    tope_minimo=maximo*0.2
-#    for precio in precios:
-#        if(precio < tope_minimo):
-#            precios.remove(precio)
+    maximo=max(precios)
+    producto.precio_maximo=maximo
+    minimo=min(precios)
+    producto.precio_minimo=minimo  
+    tope_minimo=maximo*0.2
+    for precio in precios:
+        if(precio < tope_minimo):
+            precios.remove(precio)
 
 #    producto.precio_promedio=sum(precios)/len(precios)
 #    producto.precios(precios)
